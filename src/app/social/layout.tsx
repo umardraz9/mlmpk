22cad98d97e9906799232774ddfebbf20fef36db
umardraz9 <143488862+umardraz9@users.noreply.github.com>
2025-10-19 17:16:10 +0500

    Fix social layout for dynamic rendering

--- src/app/social/layout.tsx
+++ src/app/social/layout.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+
 import React from 'react';
 import FacebookHeader from '@/components/layout/FacebookHeader';
 
