64b9e5675808ac9287927e2b56ff8676f5735a20
umardraz9 <143488862+umardraz9@users.noreply.github.com>
2025-10-19 17:08:45 +0500

    Fix dynamic pages with useSearchParams

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { Suspense, useState, useEffect, useRef, useCallback, useMemo } from 'react';
